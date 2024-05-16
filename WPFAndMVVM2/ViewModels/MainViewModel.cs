8c1c4f38d73cd750d1851306615c7055859edd68
Gustav Rasmussen <gustavarend@me.com>
2024-05-16 11:29:13 +0200

    Add project files.

---
+++ WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private PersonRepository personRepo = new PersonRepository();
+
+        private ObservableCollection<PersonViewModel> _personsVM;
+        public ObservableCollection<PersonViewModel> PersonsVM
+        {
+            get { return _personsVM; }
+            set
+            {
+                _personsVM = value;
+                OnPropertyChanged(nameof(PersonsVM));
+            }
+        }
+
+        private PersonViewModel _selectedPerson;
+        public PersonViewModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                OnPropertyChanged(nameof(SelectedPerson));
+            }
+        }
+
+        public MainViewModel()
+        {
+            SelectedPerson = null;
+            PersonsVM = new ObservableCollection<PersonViewModel>();
+
+            foreach (Person person in personRepo.GetAll())
+            {
+                PersonsVM.Add(new PersonViewModel(person));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ WPFAndMVVM2/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WPFAndMVVM2.ViewModels;
+
+namespace WPFAndMVVM2
+{
+ 
+    public partial class MainWindow : Window
+    {
+        private MainViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new MainViewModel();
+            DataContext = viewModel;
+        }
+    }
+}
